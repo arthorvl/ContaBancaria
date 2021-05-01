2418aff6fa2ba3c76a21000d8a0b0d6f2b0f1d97
Arthur Valério <joaobob63@gmail.com>
2021-05-01 18:37:19 -0300

    classe Teste

---
+++ src/ContaTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaTest {
+
+    @org.junit.jupiter.api.Test
+    void deposito() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void saque() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saldo() {
+    }
+}
